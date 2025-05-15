9bc45c3ee0ab9fced1d5eabdd862de1abfa35a0c
abdxllah <abdxllxh@hotmail.com>
2025-05-15 07:05:22 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/abdxllah/solar', // <-- your GitHub repo name here, with slashes
+  plugins: [react()],
+  base: '/solar/', // Replace 'solar' with your repository name
 });
