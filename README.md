146d28d3c3a27f330222a97c5858042db6b50ca4
abdxllah <abdxllxh@hotmail.com>
2025-08-02 16:04:43 +0100

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Simple Three js website to show off some of my projects.
--- vite.config.js
+++
@@ -1,6 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/', // the name of your repo
-});
